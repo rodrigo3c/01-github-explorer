767ad36d979532a243035cc4de7a0bbec5effbad
Rodrigo Cerqueira <55170887+rodrigo3c@users.noreply.github.com>
2021-03-11 16:03:48 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { RepositoryList } from './components/RepositoryList';
+
+import './styles/global.scss';
+
 export function App() {
-  return <h1>Hello Ignite</h1>
+  return <RepositoryList />
 }
