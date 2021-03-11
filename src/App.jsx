606bdbf8ead939f778830482b33c5dcee0457141
Rodrigo Cerqueira <55170887+rodrigo3c@users.noreply.github.com>
2021-03-11 18:05:46 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { RepositoryList } from './components/RepositoryList';
+import { Counter } from './components/Counter';
 
 import './styles/global.scss';
 
 export function App() {
-  return <RepositoryList />
+  return (
+    <>
+      <RepositoryList />
+      <Counter /> 
+    </>
+  )
+
+
 }
